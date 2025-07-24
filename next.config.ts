cc4240a2daeeee10060ce0d5c01debfe4260e751
ejim11 <favourejim56@gmail.com>
2025-07-24 17:56:57 +0100

    work

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [new URL("https://d3bcnu5rxir0tz.cloudfront.net/**")],
+    remotePatterns: [
+      new URL("https://d3bcnu5rxir0tz.cloudfront.net/**"),
+      new URL("https://d2sj7epg0gapzm.cloudfront.net/**"),
+    ],
   },
 };
 
